cc3ff6dfff5a2356f6f23699026b3422130ba378
Cody Baker <cody.c.baker.phd@gmail.com>
2024-04-29 15:22:57 -0400

    remove broken legacy c++ stuff from setup

--- setup.py
+++ setup.py
@@ -1,27 +1,6 @@
 #!/usr/bin/env python
 
-from distutils.core import setup, Extension
-from distutils.command.build_ext import build_ext
-import numpy
-import git
-
-class CustomBuildExtCommand(build_ext):
-    """build_ext command for use when numpy headers are needed."""
-
-    def run(self):
-
-        # Import numpy here, only when headers are needed
-        import numpy
-        # Add numpy headers to include_dirs
-        self.include_dirs.append(numpy.get_include())
-        
-        # Call original build_ext command
-        build_ext.run(self)
-
-_legacy_c = Extension('wormdatamodel.data._legacy_c',
-                    sources = ['wormdatamodel/data/_legacy_c.cpp'],
-                    include_dirs = [],
-                    extra_compile_args=['-O3','-D_FILE_OFFSET_BITS=64'])#
+from distutils.core import setup
                     
 # Get git commit info to build version number/tag
 #repo = git.Repo('.git')
@@ -37,5 +16,4 @@ setup(name='wormdatamodel',
       author='Francesco Randi',
       author_email='francesco.randi@gmail.com',
       packages=['wormdatamodel','wormdatamodel.data','wormdatamodel.signal'],
-      ext_modules=[_legacy_c]
      )
